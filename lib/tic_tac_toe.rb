9f9e1a5a25420d68e675bb1ee41ccad156badc84
Chris Suarez <chrissuarez001@gmail.com>
2021-06-22 01:25:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,8 +25,8 @@ def input_to_index(user_input)
 
 end
 
-def move  (board, index, current_player = "X")
-  board(index) = current_player
+def move  (input_to_index, current_player)
+  board(input_to_index) = current_player
 end
 
 
