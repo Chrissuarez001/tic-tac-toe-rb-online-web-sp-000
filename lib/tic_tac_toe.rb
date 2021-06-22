f6b0838a969529af4bf9c72aa356ebd5e5f03780
Chris Suarez <chrissuarez001@gmail.com>
2021-06-22 00:45:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,14 +11,14 @@ WIN_COMBINATIONS = [
 ]
 
 def display_board(board)
-print @board
-# print board
 
-  # puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  # puts "-----------"
-  # puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  # puts "-----------"
-  # puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+print board
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(board)
